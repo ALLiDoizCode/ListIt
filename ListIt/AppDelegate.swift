32db5bf81726f24ac3328bf3cc8dad93a51c9467
Jonathan Green <jonathangreen@Jonathans-iMac-2.local>
2015-12-08 17:04:47 -0500

    excuted parse test with sucess

--- ListIt/AppDelegate.swift
+++ ListIt/AppDelegate.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import Parse
+import Bolts
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,6 +18,17 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+        
+        Parse.enableLocalDatastore()
+        
+        // Initialize Parse.
+        Parse.setApplicationId("TNSeu9vfrUdDkyMEk5px6WKVUWNDsxmHJTTe5ukU",
+            clientKey: "6BZMO0tkYGKrDTtZTECneW8qHkWhUIHLBrDOOtyq")
+        
+        // [Optional] Track statistics around application opens.
+        PFAnalytics.trackAppOpenedWithLaunchOptions(launchOptions)
+        
+        
         return true
     }
 
