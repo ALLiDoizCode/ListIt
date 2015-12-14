c181916e0dbd110f9cab55c56d27356dd15fa355
Chris Collins <chrishcollins@fullsail.edu>
2015-12-13 21:04:20 -0500

    issues with loading data into cells

--- ListIt/UI/Viewcontrollers/ViewController.swift
+++ ListIt/UI/Viewcontrollers/ViewController.swift
@@ -8,11 +8,17 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate{
     
     let venmo:PresentVenmo = PresentVenmo()
     let user:User = User()
     let parseData:getData = getData()
+    
+    var itemData:[item] = []
+    
+   
+    //@IBOutlet weak var tableView: UITableView!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,7 +28,7 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
         
        //populate variables with data and UI binding here
-        self.venmo.getUser { (token) -> Void in
+    /*    self.venmo.getUser { (token) -> Void in
             let username = self.user.currentUser().user.username
             let userImage = self.user.currentUser().user.profileImageUrl
             print("The Token: \(token)")
@@ -30,11 +36,15 @@ class ViewController: UIViewController {
             print("User Image: \(userImage)")
             
             //reload table or collection view
-        }
+        }*/
 
         
         //parseData fill the aray for the listview items with this
         self.parseData.getItem { (items) -> Void in
+            
+            self.itemData = items
+            
+            
             
             //the images are stored as url so as not to take up memory
             print("ItemIcon: \(items[0].icon)")
@@ -44,12 +54,31 @@ class ViewController: UIViewController {
             print("Shares: \(items[0].shares)")
             print("Comments: \(items[0].comments)")
         }
-        
+        //self.tableView.reloadData()
+  
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    
+        return itemData.count
+    }
+    
+ 
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell{
+        
+        let cell:ListTableViewCell = tableView.dequeueReusableCellWithIdentifier("cell") as! ListTableViewCell
+        
+        cell.listHeadingTitle.text = itemData[indexPath.row].title
+        
+        return cell
+    
+        
     }
 
 
