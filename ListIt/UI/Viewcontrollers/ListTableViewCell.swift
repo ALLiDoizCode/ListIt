d956dd25779a9dfd10cd41219434f213b4b33e63
Jonathan Green <jonathangreen@Jonathans-iMac-2.local>
2015-12-15 01:31:06 -0500

    fixed border color ad with on cells

--- ListIt/UI/Viewcontrollers/ListTableViewCell.swift
+++ ListIt/UI/Viewcontrollers/ListTableViewCell.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class ListTableViewCell: UITableViewCell{
 
+    @IBOutlet weak var theView: UIView!
     @IBOutlet weak var listHeadingTitle: UILabel!
     @IBOutlet weak var listImage: UIImageView!
     @IBOutlet weak var userImage: UIImageView!
@@ -21,5 +22,11 @@ class ListTableViewCell: UITableViewCell{
     @IBOutlet weak var userTypeIcon: UIImageView!
     @IBOutlet weak var onlineIndicator: UIImageView!
     
-    
+    override func awakeFromNib() {
+        
+        theView.layer.borderWidth = 2
+        theView.layer.borderColor = UIColor.whiteColor().CGColor
+        theView.layer.cornerRadius = 3
+        theView.layer.masksToBounds = true
+    }
 }
