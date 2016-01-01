da12acfdd82c01b5e4416aff7f39c152a8f64987
Chris Collins <chrishcollins@fullsail.edu>
2016-01-01 01:29:20 -0500

    WIP

---
+++ ListIt/UI/Viewcontrollers/PublicProfileViewController.swift
@@ -0,0 +1,45 @@
+//
+//  PublicProfileViewController.swift
+//  ListIt
+//
+//  Created by Chris Collins on 1/1/16.
+//  Copyright © 2016 Jonathan Green. All rights reserved.
+//
+
+import UIKit
+
+class PublicProfileViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+      let cell = tableView.dequeueReusableCellWithIdentifier("public")
+        return cell!
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
